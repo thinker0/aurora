8977901ea77cd62e8b66e44b7a696f33f3685a75
Renan DelValle <rdelval1@binghamton.edu>
2016-08-04 14:08:22 -0700

    Upgrading Docker Compose

--- examples/vagrant/provision-dev-cluster.sh
+++ examples/vagrant/provision-dev-cluster.sh
@@ -89,7 +89,7 @@ EOF
 
 # Install Docker Compose
 function install_docker_compose {
-  curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 }
 
